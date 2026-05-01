8d0df42c31ea0d81f4c7f37e656b6046e4da4210
mahib-bit <sadmanmahib@gmail.com>
2026-05-02 00:12:14 +0600

    math functions

---
+++ math.js
@@ -0,0 +1,17 @@
+const min = Math.min(23 ,34, 45, 56, 67, 78, 89, 90);
+console.log(min);
+const max = Math.max(23 ,34, 45, 56, 67, 78, 89, 90);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(-5));
+console.log(Math.sqrt(16));
+console.log(Math.pow(2, 3));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.8));
+
+console.log(Math.random()); // 0 - 1
+
+const randomNum = Math.floor(Math.random() * 100); // 0 - 99
+console.log(randomNum);
